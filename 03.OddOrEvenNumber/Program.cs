d939f47fb612ab907e329b4d90f830d3dbd94cf7
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 23:32:13 +0300

    Tasks 03-04

---
+++ 03.OddOrEvenNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.OddOrEvenNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ 04.GreaterNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.GreaterNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
