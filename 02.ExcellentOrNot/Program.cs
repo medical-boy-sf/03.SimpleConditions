b8493d4ded163543d62da81c49e22203aaaac82e
Someone Else <medical_boy_sf@yahoo.com>
2018-04-16 23:25:25 +0300

    Tasks 01-02

---
+++ 02.ExcellentOrNot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.ExcellentOrNot
+{
+    class Program
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
