793b76baece34863bede559cdd99c9aa7f378834
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 14:06:21 +0300

    10-12 Tasks

---
+++ 10.Number100To200/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _10.Number100To200
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+
+            else if (num >= 100)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+
+            else
+            {
+                Console.WriteLine("Less than 100");
+            }
+        }
+    }
+}
---
+++ 11.EqualWords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.EqualWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            string wordOne = Console.ReadLine().ToLower();
+            string wordTwo = Console.ReadLine().ToLower();
+            bool equalWords = wordOne == wordTwo;
+            
+            if (equalWords)
+            {
+                Console.WriteLine("yes");
+            }
+
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
