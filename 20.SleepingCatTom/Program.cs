ff29ed024f7dd27d029ad7dbc10bf4d8793f828d
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 21:34:03 +0300

    Sleeping Cat Tom

---
+++ 20.SleepingCatTom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _20.SleepingCatTom
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberFreeDays = int.Parse(Console.ReadLine());
+            int freeDays = numberFreeDays * 127;
+            int workingDays = (365 - numberFreeDays) * 63;
+            int totalMinsPlay = freeDays + workingDays;
+            int difference = totalMinsPlay - 30000;
+            int hours = difference / 60;
+            int minutes = difference % 60;
+
+            if (difference <= 0)
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{Math.Abs(hours)} hours and {Math.Abs(minutes)} minutes less for play");
+            }
+
+            else
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+            }
+
+        }
+    }
+}
