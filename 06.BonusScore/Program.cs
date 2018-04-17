db130b2400fb95b4652c171d5429c8bb9838ce9c
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 12:35:03 +0300

    05-07 Tasks

---
+++ 06.BonusScore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.BonusScore
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            double bonus = 0;
+
+             if (num > 1000)
+            {
+                bonus = 0.1 * num;
+            }
+
+            else if (num > 100)
+            {
+                bonus = 0.2 * num;
+            }
+
+            else if (num <= 100)
+            {
+                bonus += 5;
+            }
+
+             if (num % 2 == 0)
+            {
+                bonus += 1;
+            }
+
+             if (num % 10 == 5)
+            {
+                bonus += 2;
+            }
+
+            double total = num + bonus;
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ 07.SumSeconds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.SumSeconds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int personOne = int.Parse(Console.ReadLine());
+            int personTwo = int.Parse(Console.ReadLine());
+            int personThree = int.Parse(Console.ReadLine());
+
+            int sum = personOne + personThree + personTwo;
+
+            int minutes = sum / 60;
+            int seconds = sum % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:D2}");
+
+
+        }
+    }
+}
