b57664f1b3b0d8a02197bd95dd493721ff5d6376
Someone Else <medical_boy_sf@yahoo.com>
2018-04-17 19:50:26 +0300

    Transport Price Exercise

---
+++ 18.PriceTransport/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _18.PriceTransport
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string dayOrNight = Console.ReadLine();
+            double tariff = 0;
+            double total = 0;
+
+            if (n >= 100)
+            {
+                tariff = 0.06;
+                total = n * tariff;
+            }
+
+            else if (n >= 20)
+            {
+                tariff = 0.09;
+                total = n * tariff;
+            }
+
+            else
+            {
+                if (dayOrNight == "day")
+                {
+                    tariff = 0.79;
+                    total = n * tariff + 0.7;
+                }
+                else
+                {
+                    tariff = 0.9;
+                    total = n * tariff + 0.7;
+                }
+
+            }
+
+            Console.WriteLine($"{total:F2}");
+
+        }
+    }
+}
